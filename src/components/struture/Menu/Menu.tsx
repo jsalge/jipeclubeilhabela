ebe091f929b1fef41874edc49b32ba095e113d5c
Joao Salge <jsalge82@gmail.com>
2024-10-23 17:05:54 -0300

    subindo o projeto

--- src/components/struture/Menu/Menu.tsx
+++ src/components/struture/Menu/Menu.tsx
@@ -4,12 +4,12 @@ import style from "./Menu.module.css";
 export default function Menu() {
   return (
     <Flex className={style.menu}>
-        <img className={style.logo} src="img/logo.png" alt="whatsapp" width={"200px"}/>
+        <img className={style.logo} src="img/logo.PNG" alt="whatsapp" width={"200px"}/>
         <h1>home</h1>
         <h1>pacotes</h1>
         <h1>sobre</h1>
         <h1>privativo</h1>
-        <img className={style.whatsapp} src="img/whatsapp.png" alt="whatsapp" width={"50px"}/>
+        <img className={style.whatsapp} src="img/whatsapp.PNG" alt="whatsapp" width={"50px"}/>
     </Flex>
   );
 }
